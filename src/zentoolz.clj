5d4133e530eacf433717c0f3c9d24674af98f1f3
zenathark <jcgalanh@gmail.com>
2019-06-23 22:33:26 -0500

    Add lambda macro and triple question mark

---
+++ src/zentoolz.clj
@@ -0,0 +1,14 @@
+(ns zentoolz)
+
+;;; https://github.com/klutometis/lambda
+
+(defmacro λ [& sigs] `(fn ~@sigs))
+
+(defmacro lambda [& sigs] `(fn ~@sigs))
+
+;;; end lambda
+
+(defmacro ??? []
+  `(throw
+    (ex-info "Function not yet implemented"
+             {:causes (NotImplementedException.)})))
